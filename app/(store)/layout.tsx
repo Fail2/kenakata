3b218a6ba80534c57d7e03ce8d1add4bc44c9fad
fail-2 <malaychakma7@gmail.com>
2026-09-17 11:44:18 +0600

    feat: add Drawer globally

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import CartDrawer from "@/components/cart/CartDrawer";
 import { Children } from "react";
 
 export default function StoreLayout({
@@ -10,6 +11,7 @@ export default function StoreLayout({
             <Header />
             <main className="flex-1">{children}</main>
             <Footer />
+            <CartDrawer />
         </div>
     );
 }
